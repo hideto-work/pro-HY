dbadcb06d679d74be4478dddb110a9da3669284f
hideto-work <hideto.yoshihara@uni-sax.com>
2023-09-22 16:52:51 +0900

    test1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
+
 <script setup>
-const answer=2
+import { ref } from 'vue'
+const word = ref('')
+const arr_word = ref([])
+const addToArr = () => {
+    if (word.value == '') {
+        alert('空白は登録できません！')
+    } else {
+        arr_word.value.push(word.value)
+        word.value = ''
+    }
+}
+const remToArr = (index) => {
+    arr_word.value.splice(index, 1)
+}
 </script>
 
 <template>
-<p>1+1={{ answer }}</p>
+    <input type="text" size=30 v-model="word">
+    <button @click="addToArr()">追加</button>
+    <ul v-if="arr_word.length > 0">
+        <li v-for="(val_word, i) in arr_word" :key=i>{{ i + 1 }}.{{ val_word }}　
+            <span @click="remToArr(i)" style="cursor:pointer">×</span>
+        </li>
+        <p> --- total:{{ arr_word.length }} --- </p>
+    </ul>
+    <p v-else> ※追加してください </p>
 </template>
